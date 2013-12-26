6b7be347c7285c6cf86a3bda6022a0dd40965c6a
FalconGames <falcongames109@gmail.com>
2013-12-25 22:38:14 -0500

    Update SkillType.java

--- src/com/Github/Malatak1/RPGPlus/SkillType.java
+++ src/com/Github/Malatak1/RPGPlus/SkillType.java
@@ -2,6 +2,17 @@ package com.Github.Malatak1.RPGPlus;
 
 public enum SkillType {
 	
-	STRENGTH, DEXTERITY, WISDOM, CONSTITUTION;
+	STRENGTH(10), DEXTERITY(10), WISDOM(10), CONSTITUTION(10);
+	
+	private final maxLevel;
+	
+	SkillType(int maxLevel){
+		this.maxLevel = maxLevel;
+		level = 1;
+	}
+	
+	int getMax(){
+		return maxLevel;
+	}
 	
 }
