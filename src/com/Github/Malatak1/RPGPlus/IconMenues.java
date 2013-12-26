edff8301fdca114006fbf5e7db63ddce6a896a2d
FalconGames <falcongames109@gmail.com>
2013-12-26 14:48:40 -0800

    Update IconMenues.java

--- src/com/Github/Malatak1/RPGPlus/IconMenues.java
+++ src/com/Github/Malatak1/RPGPlus/IconMenues.java
@@ -1,7 +1,7 @@
 public class IconMenues{
   private IconMenu baseIconMenu;
   private static initIconMenues(){
-    baseIconMenu = new IconMenu("Customize Character:", 9, /*get other optionclickhandler, get plugin*/, null, null);
+    baseIconMenu = new IconMenu("Customize Character:", 9, null, RPGPlus.inst());
   }
   public static getBaseIconMenu(){
     if(baseIconMenu == null) initIconMenues;
